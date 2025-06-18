2b39fff7e555caf1ebe1183edf0fe1fdf5d63e9f
Dhiren <dhirendrayogi00@gmail.com>
2025-06-19 02:26:42 +0545

    just first edit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+// function App() {
+
+import { useState } from "react";
+
+
+//   return (
+//     <div>
+//       <counter/>
+//     </div>  )
+// }
+
+const App=()=>{
+  // const clothes="shirt";
+  const[clothes,setClothes]=useState("shirt")   // state 
+  const shoes="sneakers";
+  const ChangeCothes=()=>{
+    setClothes("pants");
+
+  }
+
+
+  return(
+    <div>
+      {clothes}
+      <button onClick={ ChangeCothes}>Change clothes</button>
+        <p> "react"</p>
+    </div>
+  );
+};
+
+export default App
---
+++ src/Components/counter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const counter = () => {
+  return (
+    <div className='counter'>
+        <p>click 0 times</p>
+      <button>
+        click me
+      </button>
+
+     
+
+    
+    </div>
+  )
+}
